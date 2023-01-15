b4d200b3e276ad7e6ec3b9c345a839f7fb8e1a47
Pooja Sree <84029615+poojasree06@users.noreply.github.com>
2023-01-15 14:25:19 +0530

    updated restart game

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -3,9 +3,32 @@ using UnityEngine;
 
 public class Events : MonoBehaviour
 {
-    public void RestartGame()
-    {
+    public void Restart(){
+        int a=PlayerPrefs.GetInt("levelReached",1);
+        if(a==1){
+            RestartLevel01();
+        }else if(a==2){
+            RestartLevel02();
+        }else if(a==3){
+            RestartLevel03();
+        }else if(a==4){
+            RestartLevel04();
+        }
+    }
+    public void RestartLevel01(){
         SceneManager.LoadScene("Level01");
+    }
+
+    public void RestartLevel02(){
+        SceneManager.LoadScene("Level02");
+    }
+
+    public void RestartLevel03(){
+        SceneManager.LoadScene("Level03");
+    
+    }
+    public void RestartLevel04(){
+        SceneManager.LoadScene("Level04");
     }
 
     public void QuitGame()
